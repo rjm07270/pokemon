16cbae37e2d74555a9e58c617d54b94d1fdacc86
daniel gaertner <ransid500@outlook.com>
2020-11-28 15:46:34 -0500

    update11/28

---
+++ Pokemon/Pokemon/Blastoise.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Pokemon
+{
+    class Blastoise : Pokemon
+    {
+        //IV and EV genorater
+        private static Random RNG = new Random();
+        public Blastoise()
+
+        {
+            this.Name = "Blastoise";
+
+            this.type1 = Types.Water;
+            this.type2 = Types.None;
+
+
+            this.HPIV = RNG.Next(32);
+            this.AttackIV = RNG.Next(32);
+            this.DefenceIV = RNG.Next(32);
+            this.SpAtkIV = RNG.Next(32);
+            this.SpDefIV = RNG.Next(32);
+            this.SpeedIV = RNG.Next(32);
+
+
+            this.HPEV = 0;
+            this.AttackEV = 0;
+            this.DefenceEV = 0;
+            this.SpAtkEV = 0;
+            this.SpDefEV = 0;
+            this.SpeedEV = 0;
+
+
+            this.HP = 79;
+            this.Attack = 83;
+            this.Defence = 100;
+            this.SpAtk = 85;
+            this.SpDef = 105;
+            this.Speed = 78;
+
+            this.level = 50;
+
+
+        }
+
+    }
+}
--- Pokemon/Pokemon/Types.cs
+++ Pokemon/Pokemon/Types.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace Pokemon
 {
-    class Types
+    enum Types
     {
+        None = 0,
+        Normal = 1,
+        Fire = 2,
+        Water = 3,
+        Grass = 4,
+        Flying = 5,
+        Fighting = 6,
+        Poison = 7,
+        Electric = 8,
+        Ground = 9,
+        Rock = 10,
+        Psychic = 11,
+        Ice = 12,
+        Bug = 13,
+        Ghost = 14,
+        Steel = 15,
+        Dragon = 16,
+        Dark = 17,
+        Fairy = 18
+
     }
 }
